523e0d62bc90bf8c9bccdb3d7b8353359e112d2d
Shoeb Joarder <shoebjoarder@outlook.com>
2022-07-19 19:16:23 +0200

    added database url

--- backend/orm_interface/base.py
+++ backend/orm_interface/base.py
@@ -12,7 +12,13 @@ DB_PASS = os.environ.get('POSTGRES_PASS')
 DB_HOST = os.environ.get('POSTGRES_HOST')
 DB_PORT = os.environ.get('POSTGRES_PORT')
 DB_NAME = os.environ.get('POSTGRES_DB')
-DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+if bool(os.environ.get("POSTGRES_HOST", False)):
+    DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+else:
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    URI = DATABASE_URL.replace("postgres://", "")
+    DATABASE_URI = f'postgresql+psycopg2://{URI}'
+
 engine = create_engine(DATABASE_URI)
 Session = sessionmaker(bind=engine)
 
